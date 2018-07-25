37b093049c5d595b811a0bdab652ca514c50335a
opowell <opowell@gmail.com>
2018-07-25 14:24:31 +0200

    commit

--- build-tools/buildDocs.bat
+++ build-tools/buildDocs.bat
@@ -1,10 +1,12 @@
+REM Generate HTML help files.
+REM -------------------------
 set "htmlDir=.\client\internal\docs"
 call del %htmlDir% /Q /F
 call rmdir %htmlDir% /Q /S
 call jsdoc -c ".\build-tools\jsdoc-config.json"
 
-set "mdDir=.\client\internal\docjs-md"
-call del %mdDir% /Q /F
-call documentation build server/source/App.js -f md -o %mdDir%\app.md --github --shallow
-call documentation build server/source/Group.js -f md -o %mdDir%\group.md --github --shallow
-call documentation build server/source/** -f json -o .\client\internal\docjs.json --config build-tools/documentation.yml --github
+REM Generate JSON help content, used within the jtree editor.
+REM ---------------------------------------------------------
+set "jsonFile=.\client\internal\docjs.json"
+call del %jsonFile% /Q /F
+call documentation build server/source/** -f json -o %jsonFile%
